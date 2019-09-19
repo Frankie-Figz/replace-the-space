1bfdf0841ba882235b0c828dc48cb0a5fbd81794
Emma Stotz <stotz.emma@gmail.com>
2019-09-18 20:10:46 -0400

    Coded the JS portion. Still need to work out bugs in winning/losing condition, and update the stylesheet.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,119 @@
-// Array of space themed words
+// Various arrays
 var wordOptions = ["galaxy","neptune","blackhole", "moon","eclipse","asteroid","jupiter","astronaut","telescope","Mercury"];
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var lettersGuessed = [];
+var wordsGuessed = [];
 
 //Assigning wins/guesses left the correct number
 var wins = 0;
+var losses = 0;
 var guessesLeft = 10;
+var maxLength = 9;
+var hasHit = false;
 
 //Creating variables to hold the references to the place in the HTML where we want to display the results.
 var directions = document.getElementById ("directions");
 var wordGuessed = document.getElementById ("wordGuessed");
 var winText = document.getElementById ("winText");
+var loseText = document.getElementById ("loseText");
 var guessRemainText = document.getElementById ("guessRemainText");
 var letterGuessedText = document.getElementById ("letterGuessedText");
 
-//FUNCTION: The below will loop again after the player successfully guesses the word.
-    //Choose a word at random from the array.
-    var randomWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
+//FUNCTIONS ===========================================
 
-    //Count the number of letters in the random string choosen.
-
-    //Display "_ _ _ _" for each letter in the word in leiu of the word. FUNCTION: use a loop for this?
-
-    //Player presses any letter to begin guessing.
-    document.onkeyup = function(event) {
-
-    //Store what button the player presses.
-    var playerGuess = event.key;
-
-    //Change player's input to lower case.
-
-    //FUNCTION: use a loop to check if the player's input matches any of the letters in the string.
-
-    //CONDITION: 
-      // if the letter matches, disappear the "_" where the letter appears.
-      // else, make the letter appear under letters guessed.
-      //Move the letter from the alphabet vector to the empty vector.
-
-    //Loop until the player successfully guesses the word, and automatically restart the game with a new random selection.
+//Blank Space Function
+var blankSpaceFunction = function(word) {
+  
+  //Makes blank space visible
+  for (var v = 0; v < word.length; v++){
+    var space = document.getElementById("b" + v);
+    space.style.visibility = "visible";
+  }
+  
+  var blank = maxLength - word.length;
+  
+  //Makes blank space invisible
+  for (var h = blank + 1; h <= blank; h++){
+    var space = document.getElementById("b" + h);
+    space.style.visibility = "hidden";
+  }
 }
+
+//Valid Input Function
+var validInputFunction = function(s) {
+  // for (var i = 0; i < alphabet.length; i++){
+  //   if (input == alphabet[i]){
+  //     return true;
+  //   }
+  //   else {
+  //     return false;
+  //   }
+  // }
+  return alphabet.includes(s);
+}
+
+//Comparison Function
+var comparisonFunction = function(valid) {
+  //Get the letters within the string being guessed and ompare the letter to the letters in the array
+  for (var str = 0; str < randomWord.length; str++){
+    //Print the letter in the array to the blank space
+    if (valid == randomWord[str]) {
+      var letter = document.getElementById("b" + str);
+      letter.textContent = valid;
+      hasHit = true;
+      lettersGuessed.push(valid);
+    }
+  }
+  if (hasHit == false){
+    guessesLeft--;
+    lettersGuessed.push(valid);
+    letterGuessedText.textContent = "Letters Guessed: " + lettersGuessed.toString();
+    guessRemainText.textContent = "Guesses Left: " + guessesLeft;
+  }
+  hasHit = false;
+}
+
+//START GAME ========================================
+
+//Asks the player if they want to play the game.
+var userPlay = confirm("Do you want to play a game of Replace the Space?");
+if (userPlay == true) {
+  
+  //Get a random word from the Word Options array
+  var randomWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
+  console.log(randomWord);
+  //Send the random word to the blank space function.
+  blankSpaceFunction(randomWord);
+  
+  //Continue Guessing Condition:
+  if (guessesLeft > 0) {
+    //User presses a key:
+    document.onkeyup = function(event){
+      var userGuess = event.key;
+      var userGuessLow = userGuess.toLowerCase();
+      //Check if key pressed is valid
+      var validInput = validInputFunction(userGuessLow);
+      // console.log(validInput);
+      if (validInput == true){
+        //If valid pass for comparison to random word
+        comparisonFunction(userGuessLow);
+      }
+      //
+      
+    }
+  }
+  //Lose Condition:
+  if (guessesLeft <= 0){
+    //function that checks on the status of the game to break the code or end the game
+    losses++;
+  }
+  //Win Condition: 
+  //if blank spaces are filled, you win! 
+  //wins++;
+
+//If they player doesn't want to play, quit. 
+} else {
+  alert("Fine. I didn't want to play with you anyways.")
+}
+
+//END OF GAME =============================================================
